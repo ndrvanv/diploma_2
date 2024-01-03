0b154568843f64d0f65d0b43281ce009e7bfc9c3
Andrew <it.andr@outlook.com>
2024-01-03 19:59:39 +0300

    diploma

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': "Python Developer",
+        'location': "Moscow, Russia",
+    }
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template("home.html")
+    # jobs = load_jobs_from_db()
+    return render_template("home.html", jobs=JOBS)
+
+@app.route('/api/jobs')
+def list_jobs():
+    return jsonify(JOBS)
+
+# def load_jobs_from_db():
+#     with engine.connect() as conn:
+#         result = conn.execute(text("select * from jobs"))
+#         jobs = []
+#         for row in result.all():
+#             jobs.append(dict(row))
+#         return jobs
+#         print(result_dict)
 
 
 if __name__ == "__main__":
